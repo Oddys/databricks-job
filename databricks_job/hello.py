e14e3d6c10468436a4f53db83fa0f783a40908c8
Yurii Honcharenko <yurii_honcharenko1@epam.com>
2023-09-08 11:10:16 +0300

    Add pre-commit; Refactor

--- databricks_job/hello.py
+++ databricks_job/hello.py
@@ -4,30 +4,21 @@ from pyspark.sql.types import StructType, StringType, LongType, StructField
 
 def create_dataframe(spark: SparkSession) -> DataFrame:
     """A dummy function created just to be able to add a test."""
-    schema = StructType(
-        [
-            StructField("name", StringType()),
-            StructField("age", LongType())
-        ]
-    )
-
+    schema = StructType([StructField("name", StringType()), StructField("age", LongType())])
     df = spark.createDataFrame(
-        data=[
-            ("Alice", 30),
-            ("Bob", 40)
-        ],
+        data=[("Alice", 30), ("Bob", 40)],
         schema=schema
         # schema=("name", "age")  # just another option for providing the schema
     )
-
     return df
 
 
 def main():
-    spark = (SparkSession.builder
-             .appName("hello PySpark job")
-             # No need to specify `master` - Databricks handles that itself
-             .getOrCreate())
+    spark = (
+        SparkSession.builder.appName("hello PySpark job")
+        # No need to specify `master` - Databricks handles that itself
+        .getOrCreate()
+    )
 
     df = create_dataframe(spark)
 
@@ -35,5 +26,5 @@ def main():
     df.show()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,18 +5,13 @@ from databricks_job.hello import create_dataframe
 
 
 def test_create_dataframe(spark):
-    schema = StructType()\
-        .add("name", StringType())\
-        .add("age", LongType())
+    schema = StructType().add("name", StringType()).add("age", LongType())
     data = [
         ("Bob", 40),
         ("Alice", 30),
     ]
 
-    expected = spark.createDataFrame(
-        data=data,
-        schema=schema
-    )
+    expected = spark.createDataFrame(data=data, schema=schema)
 
     actual = create_dataframe(spark)
 
